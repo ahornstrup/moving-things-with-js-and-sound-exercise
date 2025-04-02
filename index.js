a7811964595b7bab6c5346ac710a30d3fd041b20
ahornstrup <eaa24ahor@students.eaaa.dk>
2025-04-02 13:50:55 +0200

    bada ba ba baaaa

--- index.js
+++ index.js
@@ -8,3 +8,22 @@ dodger.style.left = "175px";
 document.addEventListener("keydown", function (event) {
   console.log(event);
 });
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft();
+
+    } 
+    
+    if (event.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+
+    if (event.key === "ArrowUp") {
+        moveDodgerUp();
+    }
+
+    if (event.key === "ArrowDown") {
+        moveDodgerDown();
+    }
+});
