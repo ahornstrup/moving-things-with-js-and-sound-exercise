35c971603f08a2bb7aa0dc1923356a5b695690d4
ahornstrup <eaa24ahor@students.eaaa.dk>
2025-04-02 13:45:30 +0200

    hmmmmm

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code here
+"use strict";
+
+const dodger = document.getElementById("dodger");
+dodger.style.bottom = "175px";
+dodger.style.left = "175px";
+
+document.addEventListener("keydown", function (event) {
+  console.log(event);
+});
